e2833cb6f9a854adac9f870f4ef3f1b93812574d
EvanBrightside <xvanx84@gmail.com>
2018-10-28 19:10:47 +0300

    fix http

--- parser.rb
+++ parser.rb
@@ -9,7 +9,7 @@ TOKEN = '773528899:AAE5NKCJzEpep_2bpUY9cbUBi42N0hk7WDU'
 @user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.56 Safari/536.5'
 
 def dns_parser
-  main_url = 'https://www.dns-shop.ru/'
+  main_url = 'http://www.dns-shop.ru/'
 
   products = [
     '/b53791c627593330/processor-amd-ryzen-7-2700x-oem/',
